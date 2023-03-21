699f24aaf6ac8a6b6fa2bd67c52a8e4850a3433e
AdarshMitr <adarshmitr@gmail.com>
2023-03-21 19:54:58 +0530

    useState hook

--- reactHooks/src/App.jsx
+++ reactHooks/src/App.jsx
@@ -1,14 +1,28 @@
-
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
 
+  let msg='<Welcome World/>'
+  
+const [count, setCount]=useState(9);
+
+function decrCount(){
+  setCount(prevCount=>prevCount-1);
+}
+
+function incrCount(){
+  setCount(prevCount=>prevCount+1);
+}
   return (
-    <div className="App">
-      
-     <h1>Welcome World!</h1>
-    </div>
+     <>
+     <h1>{msg}</h1>
+     <br />
+      <button onClick={decrCount}>-</button>
+      <span> {count} </span>
+      <button onClick={incrCount}>+</button>
+     
+     </>
   )
 }
 
